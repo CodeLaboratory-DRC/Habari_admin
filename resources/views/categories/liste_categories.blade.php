8573c992ee6a5bf96df160754dde62f883fa53a8
Becky Ada <rebtshikadila@gmail.com>
2022-05-20 16:09:25 +0200

    adding ui of categories (addin categorie and list of categorie)

---
+++ resources/views/categories/liste_categories.blade.php
@@ -0,0 +1,70 @@
+@extends('templates.app')
+@section('content')
+    <div class="page-header row no-gutters py-4">
+        <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+            <span class="text-uppercase page-subtitle"></span>
+            <h3 class="page-title">Liste des Catégories</h3>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card card-small mb-4">
+                <div class="card-header border-bottom">
+                    <h6 class="m-0">Liste des catégorie</h6>
+                </div>
+                @if (session('success'))
+                    <div class="alert alert-success text-center msg" id="error">
+                        <strong>{{ session('success') }}</strong>
+                    </div>
+                @endif
+                @if (session('alert'))
+                    <div class="alert alert-success text-center msg" id="error">
+                        <strong>{{ session('alert') }}</strong>
+                    </div>
+                @endif
+                <div class="card-body p-0 pb-3 text-center">
+                    <table class="table mb-0">
+                        <thead class="bg-light">
+                            <tr>
+                                <th scope="col" class="border-0">#</th>
+                                <th scope="col" class="border-0">Nom</th>
+                                <th scope="col" class="border-0">Description</th>
+                                <th scope="col" class="border-0">Action</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if ($categories->isNotEmpty())
+                                @foreach ($categories as $categorie)
+                                    <tr>
+                                        <td>PD-{{ $categorie->id }}</td>
+                                        <td>{{ $categorie->name }}</td>
+                                        <td>{{ $categorie->overview }}</td>
+                                        <td>
+                                            <div class="btn-group" role="group" aria-label="Action Frais">
+                                                {{--<a href='{{ url("categorie/$categorie->id/edit") }}'
+                                                    class="btn btn-sm btn-primary">
+                                                    Modifier
+                                                </a>--}}
+                                                <a href='{{ url("categorie/$categorie->id/delete") }}'
+                                                    class="btn btn-sm btn-danger text-white">
+                                                    Supprimer
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <div class="alert alert-info text-center" alt="alert">
+                                    Aucune catéorie existante
+                                </div>
+                            @endif
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
